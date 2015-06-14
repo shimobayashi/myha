3a66225c9f700b65bf8f0c1a19b301fc8e5ff6e9
shimobayashi <akimasa@shimobayashi.name>
2015-06-14 16:58:16 +0900

    split script

--- led.rb
+++ led.rb
@@ -39,25 +39,3 @@ if cmd2
   p `bash -c 'echo -ne "#{cmd2}" > /dev/udp/192.168.10.16/8899'`
   p $?
 end
-
-# Send to mackerel
-epoch = Time.now.to_i
-api_key = ENV['MACKEREL_API_KEY']
-json = [
-  {
-    name: 'myself.sleep',
-    time: epoch,
-    value: settings['sleep'].to_i,
-  },
-  {
-    name: 'myself.home',
-    time: epoch,
-    value: settings['home'].to_i,
-  },
-  {
-    name: 'myself.time_to_sleep',
-    time: epoch,
-    value: settings['time_to_sleep'].to_i,
-  },
-].to_json
-`curl https://mackerel.io/api/v0/services/My-Room/tsdb -H 'X-Api-Key: #{api_key}' -H 'Content-Type: application/json' -X POST -d '#{json}'`
