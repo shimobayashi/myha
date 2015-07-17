b90f11a3c27c0bc38d5d104cdd289b0b3a3b103c
shimobayashi <akimasa@shimobayashi.name>
2015-07-17 22:41:32 +0900

    氷枕使わないとこんなもんかな

--- aircon.rb
+++ aircon.rb
@@ -2,8 +2,8 @@
 
 require 'json'
 
-AIRCON_COOLER_ON_THRESHOLD = 83.5
-AIRCON_COOLER_OFF_THRESHOLD = 80
+AIRCON_COOLER_ON_THRESHOLD = 82.5
+AIRCON_COOLER_OFF_THRESHOLD = 79
 
 def aircon_cooler_on
   `irsend SEND_ONCE aircon on_cooler_27`
