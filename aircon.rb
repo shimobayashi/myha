876990f68a3bd02777d15675f9958f567216b2ca
shimobayashi <akimasa@shimobayashi.name>
2015-07-24 08:46:11 +0900

    そんなに冷えない

--- aircon.rb
+++ aircon.rb
@@ -3,7 +3,7 @@
 require 'json'
 
 AIRCON_COOLER_ON_THRESHOLD = 81
-AIRCON_COOLER_OFF_THRESHOLD = 78
+AIRCON_COOLER_OFF_THRESHOLD = 79
 
 def aircon_cooler_on
   `irsend SEND_ONCE aircon on_cooler_27`
